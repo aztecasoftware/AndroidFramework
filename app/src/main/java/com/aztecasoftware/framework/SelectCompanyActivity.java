355ac9436cbcdaa2ff9c02f75770cb665f043de9
rcorichard <Rich45186>
2018-04-18 00:37:44 -0500

    Actualización de plugin 3.0

---
+++ app/src/main/java/com/aztecasoftware/framework/SelectCompanyActivity.java
@@ -0,0 +1,14 @@
+package com.aztecasoftware.framework;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SelectCompanyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_company);
+    }
+
+}
